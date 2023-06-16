c4bc8d79c3d16ec0eb5677638afaffcdba6953f5
ali591195 <ali.zahid3428@gmail.com>
2023-06-16 12:40:19 +0500

    Update routes.js

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -10,12 +10,12 @@ const error = require('../middleware/error');
 
 module.exports = function(app) {
     app.use(express.json());
-    app.use('/.netlify/functions/api/genres', genres);
-    app.use('/.netlify/functions/api/customers', customers);
-    app.use('/.netlify/functions/api/movies', movies);
-    app.use('/.netlify/functions/api/rentals', rentals);
-    app.use('/.netlify/functions/api/users', users);
-    app.use('/.netlify/functions/api/auth', auth);
-    app.use('/.netlify/functions/api/returns', returns);
+    app.use('/api/genres', genres);
+    app.use('/api/customers', customers);
+    app.use('/api/movies', movies);
+    app.use('/api/rentals', rentals);
+    app.use('/api/users', users);
+    app.use('/api/auth', auth);
+    app.use('/api/returns', returns);
     app.use(error);
 };
